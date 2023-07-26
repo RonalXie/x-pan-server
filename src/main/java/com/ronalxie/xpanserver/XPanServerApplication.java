c201dba98f437ed0834ef7c8766821c5028a42de
ronalxie <469563428@qq.com>
2023-07-26 16:48:24 +0800

    初始化项目

---
+++ src/main/java/com/ronalxie/xpanserver/XPanServerApplication.java
@@ -0,0 +1,13 @@
+package com.ronalxie.xpanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XPanServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XPanServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronalxie/xpanserver/XPanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronalxie.xpanserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XPanServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
